3974bd6564ea33132753fe181448fcce1efab4ae
Regine Ruud <regineruuud@gmail.com>
2022-02-02 13:17:55 +0100

    finished PCA and updated readme

--- experiments/pca/find_gender_subspace.py
+++ experiments/pca/find_gender_subspace.py
@@ -90,18 +90,20 @@ def get_bert_embeddings(tokens_tensor, segments_tensors, model):
 
 def get_embeddings_from_text(texts, gender_word, model, tokenizer):
 
-
     # Getting embeddings for the target
     # word in all given contexts
     target_word_embeddings = []
 
     for text in texts:
         tokenized_text, tokens_tensor, segments_tensors = bert_text_preparation(text, tokenizer)
+
+        print('====', tokenized_text, '=======')
+
         list_token_embeddings = get_bert_embeddings(tokens_tensor, segments_tensors, model)
 
         #print('Tokenized text: ', tokenized_text)
         
-        # Find the position 'bank' in list of tokens
+        # Find the position of gendered word in in list of tokens
         word_index = tokenized_text.index(gender_word) # = dét ordet som er i alle setningne i form av ulike kontekster
         # Get the embedding for bank
         word_embedding = list_token_embeddings[word_index]
@@ -133,12 +135,6 @@ def get_average(target_word_embeddings, han_hun):
 
 def make_numpy(vector): 
     return np.array(vector)
-
-def find_difference(han_vector, hun_vector):
-    han = make_numpy(han_vector)
-    hun = make_numpy(hun_vector)
-    diffs = hun-han
-    return diffs
 
 def get_pca_emb(emb_feat_dF, n): 
     #calculate PCA of top 10 features
@@ -175,21 +171,23 @@ def plot_scatter(principal_emb_Df):
     plt.scatter(principal_emb_Df['principal component 1'], principal_emb_Df['principal component 2'], c = np.random.rand(768), s = 10)
     plt.show()  
 
-def plot_bar(pca_emb): 
+def plot_bar(pca_emb, model_name): 
     plt.figure()
     plt.figure(figsize=(10,10))
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=14)
     plt.xlabel('Principal Component',fontsize=20)
     plt.ylabel('Percentage of explained variation',fontsize=20)
-    plt.title("Explained variation per principal component",fontsize=20)
+    plt.title("Explained variation per principal component in \n {}".format(str(model_name)),fontsize=20)
 
     x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     y = [pca_emb.explained_variance_ratio_[0], pca_emb.explained_variance_ratio_[1], pca_emb.explained_variance_ratio_[2], pca_emb.explained_variance_ratio_[3],
     pca_emb.explained_variance_ratio_[4], pca_emb.explained_variance_ratio_[5], pca_emb.explained_variance_ratio_[6], pca_emb.explained_variance_ratio_[7], 
     pca_emb.explained_variance_ratio_[8], pca_emb.explained_variance_ratio_[9]] 
     plt.bar(x, y)
-    plt.show()
+    #plt.show()
+
+    return plt
 
 def extract_sentences(filename, sheetname):
 
@@ -200,11 +198,17 @@ def extract_sentences(filename, sheetname):
 
     return han_sentences, hun_sentences
 
-def run(sentence_path, sheet_name, gender_word_pair_male, gender_word_pair_female, model_name, number_of_features):
-    han, hun = extract_sentences(filename=sentence_path, sheetname=sheet_name)
+def save_plot(plot, plot_to_filename): 
+    plot.savefig(plot_to_filename)
 
-    texts_han = [gender_word_pair_male] + han
+
+def run(sentence_path, sheet_name, gender_word_pair_male, gender_word_pair_female, model_name, number_of_features, plot_to_filename):
+   
+    han, hun = extract_sentences(filename=sentence_path, sheetname=sheet_name)
+    
     texts_hun = [gender_word_pair_female] + hun
+    texts_han = [gender_word_pair_male] + han
+    
 
     # Loading the pre-trained BERT model
     ###################################
@@ -219,45 +223,88 @@ def run(sentence_path, sheet_name, gender_word_pair_male, gender_word_pair_femal
     # embeddings to ensure consistency
     tokenizer = BertTokenizer.from_pretrained(model_name)
 
-
-    han_embeddings = make_numpy(get_embeddings_from_text(texts_han, gender_word_pair_male, model, tokenizer)).transpose()
     hun_embeddings = make_numpy(get_embeddings_from_text(texts_hun, gender_word_pair_female, model, tokenizer)).transpose()
+    han_embeddings = make_numpy(get_embeddings_from_text(texts_han, gender_word_pair_male, model, tokenizer)).transpose()
 
     diff_embeddings = han_embeddings-hun_embeddings
     #print('Diff embedding: ', diff_embeddings)
+
+    if model_name == 'ltgoslo/norbert': 
+        gutt, jente = extract_sentences(filename=sentence_path, sheetname='jente_gutt_tilfeldig')
+        texts_hun = ['jente'] + hun
+        texts_han = ['gutt'] + han
+        jente_embeddings = make_numpy(get_embeddings_from_text(texts_hun, 'jente', model, tokenizer)).transpose()
+        gutt_embeddings = make_numpy(get_embeddings_from_text(texts_han, 'gutt', model, tokenizer)).transpose()
+        diff_embeddings = (han_embeddings-hun_embeddings) + (gutt_embeddings-jente_embeddings)
 
     emb_feat_dF = get_feat_dF(diff_embeddings)
 
     pca_emb = get_pca_emb(emb_feat_dF, number_of_features)
 
     #plot results
-    plot_bar(pca_emb)
-    #plot_scatter(principal_emb_Df)
+    plot = plot_bar(pca_emb, model_name)
+    
+    #save plot as .png
+    save_plot(plot, plot_to_filename)
 
 
 
 if __name__ == '__main__':
-
+    
+    #fill in the following variables to plot pca of 
+    # top n features in sheet in .xlsx file calculated
+    # with one of the norwegian language models
+    
     NorBERT = 'ltgoslo/norbert'
     NB_BERT = 'NbAiLab/nb-bert-base'
     mBERT = 'bert-base-multilingual-cased'
 
-    #fill in the following variables to plot pca of top n features in sheet in .xlsx file calculated with one of the norwegian language models
-
+    models_list = [NorBERT, NB_BERT, mBERT]
+    name = ['NorBERT', 'NB-BERT', 'mBERT']
     sentence_path = 'experiments\pca\sample_sentences.xlsx'
-    sheet_name = 'hun_han_alle'
-    gender_word_pair_male = 'han'
-    gender_word_pair_female = 'hun'
-
-    model_name = mBERT
     number_of_features = 10
+    
+    
+    sheet_name_list = ['hun_han_alle']
+    gender_word_pair_male='han'
+    gender_word_pair_female='hun'
+    
+    for sheet in sheet_name_list: 
+        sheet_name = sheet
+        for model in models_list:
+            model_name = model
+            
+            plot_to_filename = 'experiments\pca\plots\{}_{}.png'.format(name[models_list.index(model_name)], str(number_of_features))
 
-    run(
-        sentence_path=sentence_path, 
-        sheet_name=sheet_name, 
-        gender_word_pair_male=gender_word_pair_male, 
-        gender_word_pair_female=gender_word_pair_female, 
-        model_name=model_name, 
-        number_of_features=number_of_features)
+            print('Running {} sheet with {} and target words {} and {}'.format(sheet_name, model_name, gender_word_pair_male, gender_word_pair_female))
+            run(
+                sentence_path=sentence_path, 
+                sheet_name=sheet_name, 
+                gender_word_pair_male=gender_word_pair_male, 
+                gender_word_pair_female=gender_word_pair_female, 
+                model_name=model_name, 
+                number_of_features=number_of_features, 
+                plot_to_filename=plot_to_filename)
+    """
+    sheet_name = 'jente_gutt_tilfeldig'
+    gender_word_pair_male='gutt'
+    gender_word_pair_female='jente'
+
+    for model in models_list:
+            model_name = model
+            
+            plot_to_filename = 'experiments\pca\plots\{}_{}_{}.png'.format(sheet_name, models_list.index(model_name), str(number_of_features))
+
+            print('Running {} sheet with {} and target words {} and {}'.format(sheet_name, model_name, gender_word_pair_male, gender_word_pair_female))
+            run(
+                sentence_path=sentence_path, 
+                sheet_name=sheet_name, 
+                gender_word_pair_male=gender_word_pair_male, 
+                gender_word_pair_female=gender_word_pair_female, 
+                model_name=model_name, 
+                number_of_features=number_of_features, 
+                plot_to_filename=plot_to_filename)
+    """     
+    
 
     
