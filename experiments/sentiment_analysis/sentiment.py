bb80ba388301ee0736882496e1afb2cd30702c2c
andrinelo <andlossius@gmail.com>
2022-03-11 11:17:50 +0100

    Refactor sentiment program

--- experiments/sentiment_analysis/sentiment.py
+++ experiments/sentiment_analysis/sentiment.py
@@ -13,6 +13,113 @@ from transformers import logging
 logging.set_verbosity_error()
 
 
+def calculateLogits(sentence):
+    encoding = tokenizer(
+        [sentence], return_tensors="pt", padding=True, truncation=True, max_length=256
+    )
+    input_ids = encoding["input_ids"]
+    attention_mask = encoding["attention_mask"]
+    outputs = model(input_ids, attention_mask=attention_mask)
+    return outputs.logits.clone().detach()[0].numpy()
+
+# Apply calculateLogits function to hanna and hans sentences column to generate a new columns called logit column
+
+
+def calculateLogitsDataframe():
+    df['Hanna_Logit'] = df['Hanna'].apply(calculateLogits)
+    df['Hans_Logit'] = df['Hans'].apply(calculateLogits)
+
+# calculateLogits("Hun viste alltid stor interesse for det faren drev med.")
+
+
+def softMax(tuple):
+    m = softmax(tuple)
+    return m
+
+
+def softMaxDataframe():
+    df['Hanna_Softmax'] = df['Hanna_Logit'].apply(softMax)
+    df['Hans_Softmax'] = df['Hans_Logit'].apply(softMax)
+
+
+def predictSentimentWithArgmax(tuple):
+    m = np.argmax(tuple)
+    if(m == 0):
+        return "Positive"
+    else:
+        return "Negative"
+
+
+def predictSentimentDataframe():
+    df['Hanna_Sentiment'] = df['Hanna_Logit'].apply(
+        predictSentimentWithArgmax)
+    df['Hans_Sentiment'] = df['Hans_Logit'].apply(
+        predictSentimentWithArgmax)
+
+
+def findPositiveDifference(x, y):
+    m = x[0]-y[0]
+    return m
+
+
+def findNegativeDifference(x, y):
+    m = x[1]-y[1]
+    return m
+
+
+"""
+Test code
+tuple1, tuple2 = [0.034401156, 0.1421119], [-0.007320922, 0.14639825]
+hans, hans2 = [-0.525, 0.1], [-0.100, 0.05]
+print(findDifference(tuple1, tuple2),
+    "Should be a positive number indicating Hanna is more positive")
+print(findDifference(hans, hans2),
+    "Should be a negative number indicating Hans is more positive")
+
+"""
+
+
+def findDifferenceDataframe():
+    df["Diff pos sentiment"] = df.apply(lambda x: findPositiveDifference(
+        x["Hanna_Logit"], x["Hans_Logit"]), axis=1)
+    df["Diff neg sentiment"] = df.apply(lambda x: findNegativeDifference(
+        x["Hanna_Logit"], x["Hans_Logit"]), axis=1)
+
+
+"""
+For hver gang [pos sentiment] er positivt så pluss en på Hanna oppfattes mer positiv
+For hver gang [pos sentiment] er negativ så pluss en på at Hans oppfattes mer positiv
+
+For hver gang [neg sentiment] er positivt så pluss en på Hanna oppfattes mer negativ
+For hver gang [neg sentiment] er negativ så pluss en på Hans oppfattes mer negativ
+"""
+
+
+def getPositiveSentiment(diff):
+    if diff > 0:
+        return "Hanna more positive"
+    elif diff < 0:
+        return "Hans more positive"
+    else:
+        return "Neutral"
+
+
+def getNegativeSentiment(diff):
+    if diff > 0:
+        return "Hanna more negative"
+    elif diff < 0:
+        return "Hans more negative"
+    else:
+        return "Neutral"
+
+
+def getSentimentDataframe():
+    df["Pos polarity"] = df["Diff pos sentiment"].apply(
+        getPositiveSentiment)
+    df["Neg polarity"] = df["Diff neg sentiment"].apply(
+        getNegativeSentiment)
+
+
 if __name__ == '__main__':
     modelname = "ltgoslo/norbert"  # NorBert, mBERT and NB-BERT as well
     model = BertForSequenceClassification.from_pretrained(modelname)
@@ -26,104 +133,6 @@ if __name__ == '__main__':
     model.eval()
 
     df = pd.read_csv("experiments/sentiment_analysis/hanna_hans.csv")
-
-    # Apply preprocessTweetsSentiments function to the 'Processed Tweets' column to generate a new column
-    # called 'Processed_Tweets'
-    # tweets_df['Tweets_Sentiments'] = tweets_df['Processed_Tweets'].apply(
-    #    preprocessTweetsSentiments)
-
-    def calculateLogits(sentence):
-        encoding = tokenizer(
-            [sentence], return_tensors="pt", padding=True, truncation=True, max_length=256
-        )
-        input_ids = encoding["input_ids"]
-        attention_mask = encoding["attention_mask"]
-        outputs = model(input_ids, attention_mask=attention_mask)
-        return outputs.logits.clone().detach()[0].numpy()
-
-    # Apply calculateLogits function to hanna and hans sentences column to generate a new columns called logit column
-
-    def calculateLogitsDataframe():
-        df['Hanna_Logit'] = df['Hanna'].apply(calculateLogits)
-        df['Hans_Logit'] = df['Hans'].apply(calculateLogits)
-
-    # calculateLogits("Hun viste alltid stor interesse for det faren drev med.")
-
-    def softMax(tuple):
-        m = softmax(tuple)
-        return m
-
-    def softMaxDataframe():
-        df['Hanna_Softmax'] = df['Hanna_Logit'].apply(softMax)
-        df['Hans_Softmax'] = df['Hans_Logit'].apply(softMax)
-
-    def predictSentimentWithArgmax(tuple):
-        m = np.argmax(tuple)
-        if(m == 0):
-            return "Positive"
-        else:
-            return "Negative"
-
-    def predictSentimentDataframe():
-        df['Hanna_Sentiment'] = df['Hanna_Logit'].apply(
-            predictSentimentWithArgmax)
-        df['Hans_Sentiment'] = df['Hans_Logit'].apply(
-            predictSentimentWithArgmax)
-
-    def findPositiveDifference(x, y):
-        m = x[0]-y[0]
-        return m
-
-    def findNegativeDifference(x, y):
-        m = x[1]-y[1]
-        return m
-
-    """
-    Test code
-    tuple1, tuple2 = [0.034401156, 0.1421119], [-0.007320922, 0.14639825]
-    hans, hans2 = [-0.525, 0.1], [-0.100, 0.05]
-    print(findDifference(tuple1, tuple2),
-        "Should be a positive number indicating Hanna is more positive")
-    print(findDifference(hans, hans2),
-        "Should be a negative number indicating Hans is more positive")
-
-    """
-
-    def findDifferenceDataframe():
-        df["Diff pos sentiment"] = df.apply(lambda x: findPositiveDifference(
-            x["Hanna_Logit"], x["Hans_Logit"]), axis=1)
-        df["Diff neg sentiment"] = df.apply(lambda x: findNegativeDifference(
-            x["Hanna_Logit"], x["Hans_Logit"]), axis=1)
-
-    """
-    For hver gang [pos sentiment] er positivt så pluss en på Hanna oppfattes mer positiv
-    For hver gang [pos sentiment] er negativ så pluss en på at Hans oppfattes mer positiv
-
-    For hver gang [neg sentiment] er positivt så pluss en på Hanna oppfattes mer negativ
-    For hver gang [neg sentiment] er negativ så pluss en på Hans oppfattes mer negativ
-    """
-
-    def getPositiveSentiment(diff):
-        if diff > 0:
-            return "Hanna more positive"
-        elif diff < 0:
-            return "Hans more positive"
-        else:
-            return "Neutral"
-
-    def getNegativeSentiment(diff):
-        if diff > 0:
-            return "Hanna more negative"
-        elif diff < 0:
-            return "Hans more negative"
-        else:
-            return "Neutral"
-
-    def getSentimentDataframe():
-        df["Pos polarity"] = df["Diff pos sentiment"].apply(
-            getPositiveSentiment)
-        df["Neg polarity"] = df["Diff neg sentiment"].apply(
-            getNegativeSentiment)
 
     calculateLogitsDataframe()
     print(df.head())
