d398c2c7f8e8dbe7f1639d47fb7e494ea7d55bf0
andrinelo <andlossius@gmail.com>
2022-03-14 14:18:46 +0100

    Create wordcloud for masked layer modelling

--- experiments/masked_adjectives/plot_adjectives.py
+++ experiments/masked_adjectives/plot_adjectives.py
@@ -5,14 +5,16 @@ from transformers import logging
 logging.set_verbosity_error()
 
 import pandas as pd
-#from wordcloud import WordCloud
+from wordcloud import WordCloud
 
 
 def get_data(model_name):
     df = pd.read_csv('experiments/masked_adjectives/{}_100_adjectives.csv'.format(model_name))
 
     df_male = df.iloc[:50]
-    df_female = df.iloc[50:]
+    print(df_male)
+    df_female = df.iloc[51:]
+    print(df_female)
 
     return df_male, df_female
 
@@ -22,25 +24,30 @@ def get_word_cloud_string(df):
 
     df_rounded= df.round(decimals=3)
     word_cloud_string = ''
-
+    #int(row['Differanse']*1000)
     for index, row in df_rounded.iterrows(): 
         print(row['Adjektiv'], row['Differanse'])
-        words = [row['Adjektiv'] for i in range(int(row['Differanse']*1000))]
+        words = [row['Adjektiv'] for i in range(1)]
         print(words)
         string = ' '.join(words) + ' '
         word_cloud_string += string
     return word_cloud_string
 
 
-def plot_word_cloud(word_cloud_string, model_name, male_or_female): 
-    word_cloud = WordCloud(collocations=False, background_color='white').generate(word_cloud_string)
-    word_cloud.to_file("word_cloud_{}_{}.png".format(male_or_female, model_name))
-    word_cloud.to_file("word_cloud_{}_{}.eps".format(male_or_female, model_name))
+def plot_word_cloud(word_cloud_string, model_name, male_or_female):
+    if (male_or_female == 'male'):
+        word_cloud = WordCloud(width = 250, height = 360, collocations=False, background_color='white', colormap='viridis').generate(word_cloud_string)
+        word_cloud.to_file("experiments/masked_adjectives/word_cloud_{}_{}.png".format(male_or_female, model_name))
+        word_cloud.to_file("experiments/masked_adjectives/word_cloud_{}_{}.eps".format(male_or_female, model_name))
+    else:
+        word_cloud = WordCloud(width = 250, height = 360, collocations=False, background_color='white', colormap='plasma').generate(word_cloud_string)
+        word_cloud.to_file("experiments/masked_adjectives/word_cloud_{}_{}.png".format(male_or_female, model_name))
+        word_cloud.to_file("experiments/masked_adjectives/word_cloud_{}_{}.eps".format(male_or_female, model_name))
 
 def run(df, model_name, is_male):
     if is_male ==True:  
         string = get_word_cloud_string(df)
-        print(string)
+        #print(string)
         plot_word_cloud(string, model_name, 'male')
     else: 
         string = get_word_cloud_string(df)
@@ -52,9 +59,9 @@ if __name__ == '__main__':
 
     name_list = ['NorBERT', 'NB-BERT', 'mBERT']
 
-    df_male, df_female = get_data('norbert')
+    
 
     for model_name in name_list:
-
+        df_male, df_female = get_data(model_name)
         run(df_male, model_name, True)
         run(df_female, model_name, False)
