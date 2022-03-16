50b139b8a6a8516dd17294d14604818f95f337a2
andrinelo <andlossius@gmail.com>
2022-03-16 13:29:52 +0100

    Finish experiments with sentiment analysis on gendered adjectives

--- experiments/sentiment_analysis/adjective_sentiment.py
+++ experiments/sentiment_analysis/adjective_sentiment.py
@@ -1,28 +1,82 @@
+import pandas as pd
+from transformers import BertForSequenceClassification, AutoTokenizer
+import numpy as np
+import logging
+from transformers import logging
+logging.set_verbosity_error()
 
 
-NorBERT_male_adjectives = ['kristen', 'barokk', 'kanon', 'kontrær', 'rasjonal', 'kafkask', 'demonstrativ', 'meddelsom', 'godtroende', 'prediktativ']
-NorBERT_female_adjectives = ['kvinnelig', 'følsom', 'ufødt', 'vakker', 'aktsom', 'skånsom', 'forplantningsmessig', 'søt', 'ufruktbar', 'ung']
-
-NB_BERT_male_adjectives = ['nordenfjelsk', 'nordafjelsk', 'vestlandsk', 'bergensk', 'omansk', 'algerisk', 'østre', 'fattigslig', 'søndre', 'fattigfin']
-NB_BERT_female_adjectives = ['hjemmeværende', 'prostituert', 'nybakt', 'abortiv', 'pårørende', 'midlertidig', 'redd', 'biennal', 'ufrivillig', 'bekymret']
-
-mBERT_male_adjectives = ['radial', 'flercella', 'flerdimensjonal', 'kvadratisk', 'polynomiell', 'tredimensjonal', 'gammal', 'aksial', 'geometrisk', 'elektrofysiologisk']
-mBERT_female_adjectives = ['felles', 'topp', 'neste', 'hjemmeværende', 'forhenværende', 'nær', 'full', 'vanna', 'relevant', 'framifrå']
+def get_data(model_name):
+    df_hun = pd.read_csv(
+        'experiments/sentiment_analysis/data/{}_hun_adjectives.csv'.format(model_name))
+    df_han = pd.read_csv(
+        'experiments/sentiment_analysis/data/{}_han_adjectives.csv'.format(model_name))
+    return df_hun, df_han
 
 
-def create_sentences(adj_list, is_male): 
-    if is_male: 
-        gender = 'han'
-    else: gender = 'hun'
-    
-    for i in range(len(adj_list)): 
-        adj_list[i] = gender + ' er ' + adj_list[i]
-    print(adj_list)
-    return adj_list
+def create_sentences_han(adjective):
+    return ('Han er ' + adjective)
 
-if __name__ == '__main__': 
-    NorBERT_male_sentences = create_sentences(NorBERT_male_adjectives, True)
-    NorBERT_female_sentences = create_sentences(NorBERT_female_adjectives, False)
 
-    NB_BERT_male_sentences = create_sentences(NB_BERT_male_adjectives, True)
-    NB_BERT_female_sentences = create_sentences(NB_BERT_female_adjectives, False)
+def create_sentences_hun(adjective):
+    return ('Hun er ' + adjective)
+
+
+def calculateLogits(sentence):
+    encoding = tokenizer(
+        [sentence], return_tensors="pt", padding=True, truncation=True, max_length=256
+    )
+    input_ids = encoding["input_ids"]
+    attention_mask = encoding["attention_mask"]
+    outputs = model(input_ids, attention_mask=attention_mask)
+    return outputs.logits.clone().detach()[0].numpy()
+
+
+def predictSentimentWithArgmax(tuple):
+    m = np.argmax(tuple)
+    if(m == 0):
+        return "Positive"
+    else:
+        return "Negative"
+
+
+def create_dataframe(name):
+    df_han['Complete sentence male'] = df_han['Adjektiv'].apply(
+        create_sentences_han)
+    df_hun['Complete sentence female'] = df_hun['Adjektiv'].apply(
+        create_sentences_hun)
+
+    df_hun['Female Logit'] = df_hun['Complete sentence female'].apply(
+        calculateLogits)
+    df_han['Male Logit'] = df_han['Complete sentence male'].apply(
+        calculateLogits)
+
+    df_hun['Sentiment'] = df_hun['Female Logit'].apply(
+        predictSentimentWithArgmax)
+    df_han['Sentiment'] = df_han['Male Logit'].apply(
+        predictSentimentWithArgmax)
+
+    df_hun.to_csv(
+        'experiments/sentiment_analysis/{}/adjective_analyzed_hun.csv'.format(name))
+    df_han.to_csv(
+        'experiments/sentiment_analysis/{}/adjective_analyzed_han.csv'.format(name))
+    print(df_hun.head())
+    print(df_han.head())
+
+
+if __name__ == '__main__':
+
+    name = ['NorBERT', 'NB-BERT']
+    model_list = ['ltgoslo/norbert', 'NbAiLab/nb-bert-base']
+
+    for i in range(len(model_list)):
+        print(name[i], "is the model name and",
+              model_list[i], "is the hf model name")
+        print("And those two should be equal")
+        model = BertForSequenceClassification.from_pretrained(model_list[i])
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_list[i], use_fast=False)
+        model.eval()
+
+        df_hun, df_han = get_data(name[i])
+        create_dataframe(name[i])
--- experiments/sentiment_analysis/plot.py
+++ experiments/sentiment_analysis/plot.py
@@ -32,9 +32,6 @@ def plot_pie(results, labels, title, save_to, save_to2):
 
     plt.savefig(save_to)
     plt.savefig(save_to2)
-    # plt.savefig('experiments/sentiment_analysis/results_figs/sentiment_analysis_pos_norbert.eps')
-    # plt.savefig('experiments/sentiment_analysis_neg.png')
-    # plt.savefig('experiments/sentiment_analysis_neg.eps')
 
 
 def get_and_clean_data(modelname):
@@ -43,7 +40,7 @@ def get_and_clean_data(modelname):
     df.drop(['Hans', 'Hanna_Softmax', 'Hans_Softmax', 'Diff pos sentiment',
             'Diff neg sentiment', 'Pos polarity', 'Neg polarity'], axis=1, inplace=True)
     df.columns = ['SN', 'ID2', 'Hanna/Hans Sentence', 'Hanna Logit',
-                  'Hans Logit', 'Hanna Sentiment', 'Hans Sentiment']  # ['SN' + str(i) for i in range(len(df))] instead of SN for sentence number ask Reggy
+                  'Hans Logit', 'Hanna Sentiment', 'Hans Sentiment']  # ['SN{}'.format(i) for i in range(1, len(sentences)+1)]
     df.drop(['ID2'], axis=1, inplace=True)
     return df
 
@@ -55,17 +52,104 @@ def check_sentiment(modelname):
                           df['Hans Sentiment'], '0', '1')
     new = df.loc[~(df['Hanna Sentiment'] == df['Hans Sentiment'])]
     new.drop([''])
-    print(new[])
+
+
+def get_data(name):
+    df_hun = pd.read_csv(
+        'experiments/sentiment_analysis/{}/adjective_analyzed_hun.csv'.format(name))
+    df_han = pd.read_csv(
+        'experiments/sentiment_analysis/{}/adjective_analyzed_han.csv'.format(name))
+    return df_hun, df_han
+
+
+def get_sentiment_values(name):
+    df_values_hun = df_hun[['Complete sentence female', 'Sentiment']].copy()
+    df_values_han = df_han[['Complete sentence male', 'Sentiment']].copy()
+    hun_positive = df_values_hun.Sentiment.str.count("Positive").sum()
+    hun_negative = df_values_hun.Sentiment.str.count("Negative").sum()
+    han_positive = df_values_han.Sentiment.str.count("Positive").sum()
+    han_negative = df_values_han.Sentiment.str.count("Negative").sum()
+
+    print(name)
+    print(hun_positive, hun_negative, hun_negative+hun_positive,
+          "Should sum to 431 for NB-BERT and 66 for NorBERT")
+    print(han_positive, han_negative, han_negative+han_positive,
+          "Should sum to 1263 for NB-BERT and 1549 for NorBERT")
+
+    # return hun_positive, hun_negative, han_positive, han_negative
+
+
+def plot_bar():
+    labels = ['Positive sentences', 'Negative sentences']
+    women = [430, 1]
+    men = [1258, 5]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    colours = {'Female': "#AADEA7", 'Male': "#2D87BB"}
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, men, width, label='Male', color="#2D87BB")
+    rects2 = ax.bar(x + width/2, women, width,
+                    label='Female', color='#AADEA7')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Count')
+    ax.set_title('NB-BERT')
+    ax.set_xticks(x, labels)
+    ax.legend()
+
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+
+    fig.tight_layout()
+
+    plt.savefig(
+        "experiments/sentiment_analysis/results_fig/count_adjective_NB-BERT.eps")
+    plt.savefig(
+        "experiments/sentiment_analysis/results_fig/count_adjective_NB-BERT.png")
+
+
+def plot_bar_norbert():
+    labels = ['Positive sentences', 'Negative sentences']
+    women = [12, 54]
+    men = [253, 1296]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, men, width, label='Male', color="#2D87BB")
+    rects2 = ax.bar(x + width/2, women, width, label='Female', color='#AADEA7')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Count')
+    ax.set_title('NorBERT')
+    ax.set_xticks(x, labels)
+    ax.legend()
+
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+
+    fig.tight_layout()
+
+    plt.savefig(
+        "experiments/sentiment_analysis/results_fig/count_adjective_NorBERT.eps")
+    plt.savefig(
+        "experiments/sentiment_analysis/results_fig/count_adjective_NorBERT.png")
 
 
 if __name__ == '__main__':
-    name_list = ['mBERT', 'NB-BERT', 'NorBERT']
-
-    for name in name_list:
-        print(name)
-        check_sentiment(name)
-
+    plot_bar()
+    plot_bar_norbert()
     """
+    name_list = ['NB-BERT', 'NorBERT']  # ['mBERT', 'NB-BERT', 'NorBERT']
+    for name in name_list:
+        df_hun, df_han = get_data(name)
+        get_sentiment_values(name)
+
+
     for name in name_list:
         df_positive=pd.read_csv(
             'experiments/sentiment_analysis/{}/who_is_more_positive.csv'.format(name))
